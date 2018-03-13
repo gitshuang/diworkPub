ad039e7bf00ecc894b72dc9f0df339d6bae04fc5
zhengxingcheng <xingcheng.zheng@gmail.com>
2018-03-13 14:44:19 +0800

    test

---
+++ test2/build/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _Test2 = "Text 2 Load Success!";
+
+var _TestHasDefault = _interopRequireDefault(_Test2);
+
+function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+        "default": obj
+    };
+}
+
+exports["default"] = _TestHasDefault["default"];
+module.exports = exports['default'];
