b0b1822aa2f09dd2e81542a7a8fc1561ae2a4064
zhengxingcheng <xingcheng.zheng@gmail.com>
2018-03-16 14:36:40 +0800

    build

--- build/enhancedPagination/index.js
+++ build/enhancedPagination/index.js
@@ -17,10 +17,10 @@ var _pagination2 = _interopRequireDefault(_pagination);
 require('./style.css');
 
 var _style = {
-  'enhanced_pagination': 'enhanced_pagination__style___3adls',
-  'data_select': 'data_select__style___3IIDe',
-  'page_jump': 'page_jump__style___2UQbq',
-  'page_jump_value': 'page_jump_value__style___2gtGd'
+  'enhanced_pagination': 'enhanced_pagination__style___3LhAI',
+  'data_select': 'data_select__style___1N0jE',
+  'page_jump': 'page_jump__style___3AeGw',
+  'page_jump_value': 'page_jump_value__style___15gy0'
 };
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
