1bbbaf458d45dff5f234d5ee6bc0f72ae6ae95d7
zhengxingcheng <xingcheng.zheng@gmail.com>
2018-03-16 14:18:56 +0800

    优化按钮组件

--- build/button/index.js
+++ build/button/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
-exports.ButtonDefaultWhite = exports.ButtonCheckSelected = exports.ButtonCheckClose = exports.ButtonWarning = exports.ButtonDefaultLine = exports.ButtonDefaultAlpha = exports.ButtonBrand = exports.ButtonDanger = undefined;
+exports.ButtonDefaultWhite = exports.ButtonCheckSelected = exports.ButtonCheckClose = exports.ButtonWarning = exports.ButtonDefaultLine = exports.ButtonDefaultAlpha = exports.ButtonDefault = exports.ButtonBrand = exports.ButtonDanger = undefined;
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
@@ -15,179 +15,96 @@ var _button = require('../bee/button');
 
 var _button2 = _interopRequireDefault(_button);
 
+var _utils = require('../utils');
+
+var _utils2 = _interopRequireDefault(_utils);
+
 require('./style.css');
 
 var _style = {
-    'btn': 'btn__style___30SNS',
-    'brand_btn': 'brand_btn__style___oBMdh',
-    'default_btn': 'default_btn__style___3x4sO',
-    'default_line_btn': 'default_line_btn__style___1OFoh',
-    'default_alpha_btn': 'default_alpha_btn__style___1KAkZ',
-    'default_white_btn': 'default_white_btn__style___C1i1t',
-    'check_selected_btn': 'check_selected_btn__style___2SHDD',
-    'disabled_check_selected_btn': 'disabled_check_selected_btn__style___2wR5o',
-    'check_close_btn': 'check_close_btn__style___3mcm-',
-    'disabled_check_close_btn': 'disabled_check_close_btn__style___24bnq',
-    'danger_btn': 'danger_btn__style___2INSY',
-    'disabled_danger_btn': 'disabled_danger_btn__style___3YX1Q',
-    'warning_btn': 'warning_btn__style___2Bths'
+  'btn': 'btn__style___37bsb',
+  'brand_btn': 'brand_btn__style___2q2wL',
+  'default_btn': 'default_btn__style___ID6yW',
+  'default_line_btn': 'default_line_btn__style___bm6bw',
+  'default_alpha_btn': 'default_alpha_btn__style___LfXeC',
+  'default_white_btn': 'default_white_btn__style___3sPDO',
+  'check_selected_btn': 'check_selected_btn__style___4yC4T',
+  'disabled_check_selected_btn': 'disabled_check_selected_btn__style___9vvvC',
+  'check_close_btn': 'check_close_btn__style___IV5Sb',
+  'disabled_check_close_btn': 'disabled_check_close_btn__style___2iKOo',
+  'danger_btn': 'danger_btn__style___3JHoK',
+  'disabled_danger_btn': 'disabled_danger_btn__style___3mBiP',
+  'warning_btn': 'warning_btn__style___3xJGo'
 };
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+function _defaults(obj, defaults) { var keys = Object.getOwnPropertyNames(defaults); for (var i = 0; i < keys.length; i++) { var key = keys[i]; var value = Object.getOwnPropertyDescriptor(defaults, key); if (value && value.configurable && obj[key] === undefined) { Object.defineProperty(obj, key, value); } } return obj; }
+
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : _defaults(subClass, superClass); }
+
+var buttonMaker = function buttonMaker(btnType) {
+  var _class, _temp;
+
+  return _temp = _class = function (_Component) {
+    _inherits(_class, _Component);
+
+    function _class() {
+      _classCallCheck(this, _class);
+
+      return _possibleConstructorReturn(this, _Component.apply(this, arguments));
+    }
+
+    _class.prototype.render = function render() {
+      var _props = this.props,
+          className = _props.className,
+          _onClick = _props.onClick;
+
+      return _react2["default"].createElement(_button2["default"], _extends({
+        className: _style.btn + ' ' + btnType + ' ' + className,
+        disabled: disabled,
+        onClick: function onClick(e) {
+          _onClick.apply(undefined, [e].concat(_toConsumableArray(props)));
+        }
+      }, this.props));
+    };
+
+    return _class;
+  }(_react.Component), _class.defaultProps = {
+    className: '',
+    onClick: _utils2["default"]
+  }, _temp;
+};
 
 //品牌色
-var ButtonBrand = function ButtonBrand(_ref) {
-    var props = _objectWithoutProperties(_ref, []);
-
-    var children = props.children,
-        disabled = props.disabled,
-        className = props.className;
-
-    return _react2["default"].createElement(
-        _button2["default"],
-        { className: _style.btn + ' ' + _style.brand_btn + ' ' + (className ? className : ''), disabled: disabled, onClick: function onClick(e) {
-                props.onClick.apply(props, [e].concat(_toConsumableArray(props)));
-            } },
-        children
-    );
-};
-
+var ButtonBrand = buttonMaker(_style.brand_btn);
 //通用按钮
-var ButtonDefault = function ButtonDefault(_ref2) {
-    var props = _objectWithoutProperties(_ref2, []);
-
-    var children = props.children,
-        disabled = props.disabled;
-
-    return _react2["default"].createElement(
-        _button2["default"],
-        { className: _style.btn + ' ' + _style.default_btn, disabled: disabled, onClick: function onClick(e) {
-                props.onClick.apply(props, [e].concat(_toConsumableArray(props)));
-            } },
-        children
-    );
-};
-
+var ButtonDefault = buttonMaker(_style.default_btn);
 //带边框
-var ButtonDefaultLine = function ButtonDefaultLine(_ref3) {
-    var props = _objectWithoutProperties(_ref3, []);
-
-    var children = props.children,
-        disabled = props.disabled;
-
-    return _react2["default"].createElement(
-        _button2["default"],
-        { className: _style.btn + ' ' + _style.default_line_btn, type: 'button', disabled: disabled, onClick: function onClick(e) {
-                props.onClick.apply(props, [e].concat(_toConsumableArray(props)));
-            } },
-        children
-    );
-};
-
+var ButtonDefaultLine = buttonMaker(_style.default_line_btn);
 //默认背景透明
-var ButtonDefaultAlpha = function ButtonDefaultAlpha(_ref4) {
-    var props = _objectWithoutProperties(_ref4, []);
-
-    var children = props.children,
-        disabled = props.disabled;
-
-    return _react2["default"].createElement(
-        _button2["default"],
-        { className: _style.btn + ' ' + _style.default_alpha_btn, disabled: disabled, onClick: function onClick(e) {
-                props.onClick.apply(props, [e].concat(_toConsumableArray(props)));
-            } },
-        children
-    );
-};
-
+var ButtonDefaultAlpha = buttonMaker(_style.default_alpha_btn);
 //默认背景透明
-var ButtonCheckSelected = function ButtonCheckSelected(_ref5) {
-    var props = _objectWithoutProperties(_ref5, []);
-
-    var children = props.children,
-        disabled = props.disabled,
-        id = props.id;
-
-    var attribute = id ? { id: id } : {};
-    return _react2["default"].createElement(
-        _button2["default"],
-        _extends({}, attribute, { className: _style.btn + ' ' + _style.check_selected_btn, disabled: disabled, onClick: function onClick(e) {
-                props.onClick.apply(props, [e].concat(_toConsumableArray(props)));
-            } }),
-        children
-    );
-};
+var ButtonCheckSelected = buttonMaker(_style.check_selected_btn);
 //默认背景透明
-var ButtonCheckClose = function ButtonCheckClose(_ref6) {
-    var props = _objectWithoutProperties(_ref6, []);
-
-    var children = props.children,
-        disabled = props.disabled;
-
-    return _react2["default"].createElement(
-        _button2["default"],
-        { className: _style.btn + ' ' + _style.check_close_btn, disabled: disabled, onClick: function onClick(e) {
-                props.onClick.apply(props, [e].concat(_toConsumableArray(props)));
-            } },
-        children
-    );
-};
-
+var ButtonCheckClose = buttonMaker(_style.check_close_btn);
 //默认白色背景带边框
-var ButtonDefaultWhite = function ButtonDefaultWhite(_ref7) {
-    var props = _objectWithoutProperties(_ref7, []);
-
-    var children = props.children,
-        disabled = props.disabled;
-
-    return _react2["default"].createElement(
-        _button2["default"],
-        { className: _style.btn + ' ' + _style.default_white_btn, disabled: disabled, onClick: function onClick(e) {
-                props.onClick.apply(props, [e].concat(_toConsumableArray(props)));
-            } },
-        children
-    );
-};
-
+var ButtonDefaultWhite = buttonMaker(_style.default_white_btn);
 //危险
-var ButtonDanger = function ButtonDanger(_ref8) {
-    var props = _objectWithoutProperties(_ref8, []);
-
-    var children = props.children,
-        disabled = props.disabled;
-
-    return _react2["default"].createElement(
-        _button2["default"],
-        { className: _style.btn + ' ' + _style.danger_btn, disabled: disabled, onClick: function onClick(e) {
-                props.onClick.apply(props, [e].concat(_toConsumableArray(props)));
-            } },
-        children
-    );
-};
-
+var ButtonDanger = buttonMaker(_style.danger_btn);
 //警告
-var ButtonWarning = function ButtonWarning(_ref9) {
-    var props = _objectWithoutProperties(_ref9, []);
-
-    var children = props.children,
-        disabled = props.disabled;
-
-    return _react2["default"].createElement(
-        _button2["default"],
-        { className: _style.btn + ' ' + _style.warning_btn, disabled: disabled, onClick: function onClick(e) {
-                props.onClick.apply(props, [e].concat(_toConsumableArray(props)));
-            } },
-        children
-    );
-};
+var ButtonWarning = buttonMaker(_style.warning_btn);
 
 exports["default"] = ButtonDefault;
 exports.ButtonDanger = ButtonDanger;
 exports.ButtonBrand = ButtonBrand;
+exports.ButtonDefault = ButtonDefault;
 exports.ButtonDefaultAlpha = ButtonDefaultAlpha;
 exports.ButtonDefaultLine = ButtonDefaultLine;
 exports.ButtonWarning = ButtonWarning;
