6fd4295df318ae95a351abdf0566361fd1660da9
zhengxingcheng <xingcheng.zheng@gmail.com>
2018-03-30 10:52:57 +0800

    bugfix
    * 修改euc名字

--- build/utils.js
+++ build/utils.js
@@ -114,16 +114,12 @@ var getHost = exports.getHost = function getHost() {
       development: ''
     },
     euc: {
-      production: 'https://euc.yonyoucloud.com',
+      production: 'https://euc.diwork.com',
       development: 'https://idtest.yyuap.com'
     },
     market: {
       production: 'https://uastest.yyuap.com/market/index#/shopping/orderlist',
       development: 'https://uastest.yyuap.com/market/index#/shopping/orderlist'
-    },
-    editAvatar: {
-      production: 'https://euc.diwork.com/diuser',
-      development: 'https://idtest.yyuap.com/usercenter/user'
     }
   };
   return hosts[key][process.env.NODE_ENV];
