91eb1f1456accb37fb8722686e661db2baf3677a
xyoooo <yaoxinc@yonyou.com>
2018-08-06 11:29:25 +0800

    将组件中的中文提取出来

--- src/enhancedPagination/index.js
+++ src/enhancedPagination/index.js
@@ -27,6 +27,11 @@ const EnhancedPagination = WrappedComponent => {
         ],
         items: 0,
         activePage: 1,
+        dataNum:undefined,
+        enhancedPaginationText:{//因为多语
+          jump:'跳至',
+          jumpPage:'页'
+        }
       }
 
       constructor(props){
@@ -68,7 +73,7 @@ const EnhancedPagination = WrappedComponent => {
           dataNum:value
         })
         if(this.props.onDataNumSelect){
-          this.props.onDataNumSelect(e.target.value,dataNumValue)
+          this.props.onDataNumSelect(value,dataNumValue)
         }
       }
 
@@ -77,19 +82,19 @@ const EnhancedPagination = WrappedComponent => {
             maxButtons:5,
             boundaryLinks:true
         }
-        const {onDataNumSelect,dataNumSelect, ...restProps} = this.props
+        const {onDataNumSelect,dataNumSelect,dataNum, enhancedPaginationText, ...restProps} = this.props
         return (
             <div className={enhanced_pagination}>
                 <WrappedComponent  {...newProps} {...restProps}  className={u_float_pagination}/>
                 <div className={data_per_select}>
-                    <select  name="data-select" id="" className={data_select}  value={this.state.dataNum} onChange={e=>this.dataNumSelect(e)}>
+                    <select  name="data-select" id="" className={data_select}  value={dataNum===undefined?this.state.dataNum:dataNum} onChange={e=>this.dataNumSelect(e)}>
                       {dataNumSelect.length > 0 && dataNumSelect.map((item, i) => {
                       return <option key={i} value={item.id}>{item.name}</option>
                       })}
                     </select>
                 </div>
                 <div className={page_jump}>
-                    跳至<input className={page_jump_value} type='number' value={this.state.activePage} onKeyDown={e=>this.onKeyup(e)} onChange={e=>this.setPageJump(e) }/>页
+                    {enhancedPaginationText.jump}<input className={page_jump_value} type='number' value={this.state.activePage} onKeyDown={e=>this.onKeyup(e)} onChange={e=>this.setPageJump(e) }/>{enhancedPaginationText.jumpPage}
                 </div>
             </div>
         )
