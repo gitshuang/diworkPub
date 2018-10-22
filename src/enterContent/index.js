a13b8120319aead860b5326dde7f6e80adeef1d0
xingychb <xingychb@yonyou.com>
2018-10-22 16:23:23 +0800

    增加参数

--- src/enterContent/index.js
+++ src/enterContent/index.js
@@ -188,7 +188,7 @@ class EnterContent extends Component {
         return obj;
       }, {});
 
-      handleClickFn(param, ({ error, payload }) => {
+      handleClickFn(param, _from,({ error, payload }) => {
         // 只要是回调都将按钮的disabled 设定为false
         this.setState({
           disabled: false,
