cde42dfe28461bf766b47cfc672281b79a5fdfc0
xingychb <xingychb@yonyou.com>
2019-05-20 15:43:03 +0800

    ok

--- build/bee/button.js
+++ build/bee/button.js
@@ -1,32 +1,37 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 exports["default"] = undefined;
 
 var _beeButton = require('bee-button');
 
 Object.keys(_beeButton).forEach(function (key) {
-  if (key === "default" || key === "__esModule") return;
-  Object.defineProperty(exports, key, {
-    enumerable: true,
-    get: function get() {
-      return _beeButton[key];
-    }
-  });
+    if (key === "default" || key === "__esModule") return;
+    Object.defineProperty(exports, key, {
+        enumerable: true,
+        get: function get() {
+            return _beeButton[key];
+        }
+    });
 });
 Object.defineProperty(exports, 'default', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_beeButton)["default"];
-  }
+    enumerable: true,
+    get: function get() {
+        return _interopRequireDefault(_beeButton)["default"];
+    }
 });
 
 require('bee-button/build/Button.css');
 
-require('./button.css');
+var _ = require('..utils');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+if (window.location.origin.indexOf("u8c") > -1) {
+    require("./button_u8c.css");
+} else {
+    require("./button.css");
+}
 module.exports = exports['default'];
--- src/bee/button.js
+++ src/bee/button.js
@@ -1,4 +1,10 @@
 import 'bee-button/build/Button.css';
-import './button.css';
+import { getContext } from '..utils'
+
+if (window.location.origin.indexOf("u8c") > -1) {
+    require("./button_u8c.css");
+} else {
+    require("./button.css");
+}
 export * from 'bee-button';
 export { default } from 'bee-button';
