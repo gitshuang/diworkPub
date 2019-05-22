d0456fa260b222f1fbf0ad619c49d56a38ea4368
wuxlr <wuxlr@yonyou.com>
2019-05-22 16:19:12 +0800

    修改树、表格、页签样式

--- src/bee/table.js
+++ src/bee/table.js
@@ -1,5 +1,8 @@
 import 'bee-table/build/Table.css';
-import './table.css'
+if (window.location.origin.indexOf("u8c") > -1) {
+  require("./table_u8c.css");
+}
+
 /*
 * 行选中状态，添加selected类名
 * 操作按钮的父元素用action-btns-container类名，按钮添加action-btn
--- src/bee/tree.js
+++ src/bee/tree.js
@@ -1,4 +1,6 @@
 import 'bee-tree/build/Tree.css';
-
+if (window.location.origin.indexOf("u8c") > -1) {
+  require("./tree_u8c.css");
+}
 export * from 'bee-tree';
 export { default } from 'bee-tree';
