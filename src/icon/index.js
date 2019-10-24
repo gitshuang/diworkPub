3da6c23773743124ac39aa574b049c066b08298e
xingychb <xingychb@yonyou.com>
2019-10-24 10:46:31 +0800

    字体图标优化

--- src/icon/index.js
+++ src/icon/index.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import types from './style.css';//font字体库
 import sizes from './index.css';
-
-import fonts from './font/iconfont.css';
+import types from './style.css';//icomonn字体库
+import fonts from './font/iconfont.css';// iconfont字体
 
 const propTypes = {
   type: PropTypes.string,
